6d5e5f98e5b7d82feb956f54f9170f2445178933
Ohad Doron <ogdoron@gmail.com>
2022-05-24 23:22:38 +0300

    data handler updates

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,35 +1,53 @@
+import csv
 import hashlib
+import json
+from abc import ABC
+from pathlib import Path
 from typing import List, Dict
 
+import typer
+from loguru import logger
+import numpy as np
 import redis
 import pymongo
+from typer import Typer
+
+app = Typer(name='Data Handler')
 
 
-class DataHandler:
-    def __init__(self, redis_connection_string: str = None, mongodb_connection_string: str = None, db_name: str = None,
+class AbstractDataHandler(ABC):
+    def __init__(self, redis_host: str = None, mongodb_connection_string: str = None, db_name: str = None,
                  cache: bool = False,
-                 force_cache: bool = False):
+                 force_cache: bool = False,
+                 redis_port: int = 6379):
         self.force_cache = force_cache
-        if redis_connection_string is None and mongodb_connection_string is None:
+        if redis_host is None and mongodb_connection_string is None:
             raise ValueError('Must provide either redis connection string or mongo connection string, or both but not '
                              'neither')
 
         if mongodb_connection_string is not None and db_name is None:
             raise ValueError('Cannot provide mongodb connection string and not provide database name')
 
-        if redis_connection_string:
-            self._redis = redis.Redis(redis_connection_string)
+        if redis_host:
+            self._redis = redis.Redis(host=redis_host, port=redis_port)
         else:
             self._redis = dict()
 
         self.cache = cache if self._redis else False  # allows caching if redis is available
 
         if mongodb_connection_string:
-            self._db = pymongo.MongoClient(mongodb_connection_string)[db_name]
+            client = pymongo.MongoClient(mongodb_connection_string)
+            logger.debug(client.server_info())
+            self._db = client[db_name]
+            logger.debug(self)
         else:
             self._db = None
 
+
+class PatientDataHandler(AbstractDataHandler):
+
     def fetch_patient_specific_data(self, patient: str, modality: str, features: List[str]):
+        logger.debug(f'Fetching patient info for {features}')
         hashes = {
             feature: self.generate_patient_modality_feature_hash(patient=patient, modality=modality, feature=feature)
             for feature in
@@ -38,8 +56,11 @@ class DataHandler:
         results = self.get_patient_info_from_redis(hashes)
 
         if not any([item['value'] for item in results]):
+            logger.debug(f'Some features were missing in Redis for patient {patient}, looking up in MongoDB')
             results = self.get_patient_info_from_mongo(patient=patient, modality=modality, features=features)
+            logger.debug(f'MongoDB query for patient {patient} completed')
             if self.cache:
+                logger.debug(f'Filling in missing information in Redis for patient: {patient}')
                 self.set_patient_specific_data(patient=patient,
                                                modality=modality,
                                                features={item['feature']: item['value'] for item in results}
@@ -97,20 +118,206 @@ class DataHandler:
     def generate_patient_modality_feature_hash(patient: str, modality: str, feature: str) -> str:
         return f'{patient}-{modality}-{feature}'
 
-    def generate_feature_modality_hash(self, modality: str, feature: str) -> str:
+
+class FeatureDataHandler(AbstractDataHandler):
+    @staticmethod
+    def generate_feature_modality_hash(modality: str, feature: str) -> str:
         return f'{modality}-{feature}'
 
-    def get_feature_specific_data(self, feature: str, modality: str):
-        hash = self.generate_feature_modality_hash(modality=modality, feature=feature)
+    def get_feature_info_from_redis(self, hsh):
+        if self.force_cache:
+            return None
+        else:
+            return self._redis.get(hsh)
 
-        results = self.get_patient_info_from_redis(hashes)
+    def get_feature_info_from_mongo(self, modality: str, feature: str) -> np.ndarray:
+        return np.array(
+            [item['value'] for item in self._db[modality].aggregate(
+                [
+                    {
+                        '$match': {
+                            'name': feature
+                        }
+                    }, {
+                    '$project': {
+                        'feature': '$name',
+                        'value': '$value',
+                        '_id': 0
+                    }
+                }
+                ]
+            )
+             ]
+        )
 
-        if not any([item['value'] for item in results]):
-            results = self.get_patient_info_from_mongo(patient=patient, modality=modality, features=features)
+    def fetch_feature_specific_data(self, feature: str, modality: str):
+        hsh = self.generate_feature_modality_hash(modality=modality, feature=feature)
+
+        result = self.get_feature_info_from_redis(hsh)
+
+        if result is None:
+            result = self.get_feature_info_from_mongo(modality=modality, feature=feature)
             if self.cache:
-                self.set_patient_specific_data(patient=patient,
-                                               modality=modality,
-                                               features={item['feature']: item['value'] for item in results}
-                                               )
+                self.set_feature_specific_data(
+                    feature=feature,
+                    modality=modality,
+                    value=result
+                )
 
+        return result
+
+    def set_feature_specific_data(self, modality: str, feature: str, value: np.ndarray):
+        self._redis[self.generate_feature_modality_hash(modality=modality, feature=feature)] = value
+
+
+class FeaturesMetadataHandler(AbstractDataHandler):
+    def fetch_features_metadata(self, modality: str, features: List[str], patients: List[str] = None):
+        results: list = self.get_features_metadata_from_redis(modality, features)
+        missing_features = [result['feature'] for result in results if result['metadata'] is None]
+        if not missing_features:
+            return results
+
+        results = [result for result in results if result['metadata'] is not None]
+
+        missing_data = self.get_features_metadata_from_mongo(modality=modality,
+                                                             features=missing_features,
+                                                             patients=patients)
+        if self.cache:
+            [self.set_feature_metadata(modality=modality, feature=data['feature'], value=data['metadata']) for data in
+             missing_data]
+        results += missing_data
         return results
+
+    def set_feature_metadata(self, modality: str, feature: str, value: dict):
+        self._redis[self.generate_feature_modality_metadata_hash(modality=modality, feature=feature)] = json.dumps(
+            value)
+
+    def get_features_metadata_from_redis(self, modality, features: List[str]) -> List[dict]:
+        if self.force_cache:
+            return [dict(feature=feature, metadata=None) for feature in features]
+        features_metadata = []
+        for feature in features:
+            metadata = self._redis.get(
+                self.generate_feature_modality_metadata_hash(
+                    modality=modality,
+                    feature=feature
+                )
+            )
+            if metadata:
+                metadata = json.loads(metadata)
+            features_metadata.append(
+                dict(
+                    feature=feature,
+                    metadata=metadata
+                )
+            )
+        return features_metadata
+
+    def get_features_metadata_from_mongo(self, modality: str, features: List[str], patients: List[str] = None):
+        if features is None:
+            features = list(self._db[modality].distinct('name'))
+        return [dict(feature=item['feature'], metadata=item['metadata']) for item in list(self._db[modality].aggregate([
+            {
+                '$match': {
+                    'name': {
+                        '$in':
+                            features
+                    },
+                    'patient': {
+                        '$in':
+                            patients
+                    }
+                } if patients else {'name': {
+                    '$in':
+                        features
+                }, 'patient': {'$ne': None}}
+            }, self.get_metadata_aggregation_values(), {
+                '$addFields': {
+                    'var': {
+                        '$pow': [
+                            '$stdev', 2
+                        ]
+                    }
+                }
+            }, {
+                '$project': {
+                    'metadata': {
+                        'max': '$max',
+                        'min': '$min',
+                        'stdev': '$stdev',
+                        'avg': '$avg',
+                        'var': {
+                            '$pow': [
+                                '$stdev', 2
+                            ]
+                        }
+                    },
+                    'feature': '$_id',
+                    '_id': 0
+                }
+            }
+        ]))]
+
+    @staticmethod
+    def generate_feature_modality_metadata_hash(modality: str, feature: str) -> str:
+        return f'{modality}-{feature}-metadata'
+
+    def get_metadata_aggregation_values(self):
+        return {
+            '$group': {
+                '_id': '$name',
+                'max': {
+                    '$max': '$value'
+                },
+                'min': {
+                    '$min': '$value'
+                },
+                'stdev': {
+                    '$stdDevPop': '$value'
+                },
+                'avg': {
+                    '$avg': '$value'
+                }
+            }
+        }
+
+
+@app.command()
+def generate_metadata(
+        mongodb_connection_string: str = typer.Option(..., help='MongoDB connection string to draw data from',
+                                                      prompt_required=True),
+        db_name: str = typer.Option(..., help='Database name', prompt_required=True),
+        modality: str = typer.Option(..., help='Modality to compute metadata for'),
+        redis_host: str = typer.Option(...,
+                                       help='Redis connection string. Metadata will be automatically '
+                                            'inserted'),
+        patients_file: Path = typer.Option(None,
+                                           help='Path to a csv file containing patients in comma seperated values for '
+                                                'patients to be used when computing metadata. If not provided, '
+                                                'will compute for all patients',
+                                           exists=True, file_okay=True, readable=True, resolve_path=True),
+        features_file: Path = typer.Option(None,
+                                           help='Path to a csv file containing features in comma seperated values for '
+                                                'patients to be used when computing metadata. If not provided, '
+                                                'will compute for all features',
+                                           exists=True, file_okay=True, readable=True, resolve_path=True),
+        force_override: bool = typer.Option(False, help='If True, will override the data already existing in Redis')
+):
+    if patients_file:
+        patients = list(csv.reader(patients_file.open()))[0]
+    else:
+        patients = None
+    if features_file:
+        features = list(csv.reader(features_file.open()))[0]
+    else:
+        features = None
+    return FeaturesMetadataHandler(mongodb_connection_string=mongodb_connection_string,
+                                   db_name=db_name,
+                                   redis_host=redis_host,
+                                   force_cache=force_override,
+                                   cache=True
+                                   ).fetch_features_metadata(patients=patients, features=features, modality=modality)
+
+
+if __name__ == '__main__':
+    app()
--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -1,11 +1,15 @@
+import csv
+from tempfile import TemporaryDirectory
+
 import mongomock
+import numpy as np
 import pymongo
 import pytest
 from pathlib import Path
 import redis
 from bson import json_util
 
-from src.data_handler import DataHandler
+from src.data_handler import PatientDataHandler, FeatureDataHandler, FeaturesMetadataHandler, generate_metadata
 from unittest.mock import patch, MagicMock
 
 
@@ -15,17 +19,36 @@ def insert_data_to_database():
         client['TCGAOmics']['mRNA'].insert_many(items)
 
 
-def get_data_handler(cache: bool = False, force_cache: bool = False) -> DataHandler:
-    return DataHandler(mongodb_connection_string='mongodb://localhost:27017', db_name='TCGAOmics',
-                       redis_connection_string='redis://localhost:6379', cache=cache, force_cache=force_cache)
+def get_patient_data_handler(cache: bool = False, force_cache: bool = False) -> PatientDataHandler:
+    return PatientDataHandler(mongodb_connection_string='mongodb://localhost:27017',
+                              db_name='TCGAOmics',
+                              redis_connection_string='redis://localhost:6379',
+                              cache=cache,
+                              force_cache=force_cache)
+
+
+def get_feature_data_handler(cache: bool = False, force_cache: bool = False) -> FeatureDataHandler:
+    return FeatureDataHandler(mongodb_connection_string='mongodb://localhost:27017',
+                              db_name='TCGAOmics',
+                              redis_connection_string='redis://localhost:6379',
+                              cache=cache,
+                              force_cache=force_cache)
+
+
+def get_features_metadata_handler(cache: bool = False, force_cache: bool = False) -> FeaturesMetadataHandler:
+    return FeaturesMetadataHandler(mongodb_connection_string='mongodb://localhost:27017',
+                                   db_name='TCGAOmics',
+                                   redis_connection_string='redis://localhost:6379',
+                                   cache=cache,
+                                   force_cache=force_cache)
 
 
 @patch.object(redis, 'Redis', return_value=dict(abc=True))
-class TestDataHandler:
+class TestPatientDataHandler:
     @mongomock.patch(servers=('mongodb://localhost:27017'))
     def test_fetch_patient_specific_data(self, *args):
         insert_data_to_database()
-        data_handler = get_data_handler()
+        data_handler = get_patient_data_handler()
         features = data_handler.fetch_patient_specific_data(patient='TCGA-06-2557', modality='mRNA',
                                                             features=['TSPAN6'])
         assert features == [{'feature': 'TSPAN6', 'value': 14.05}]
@@ -37,8 +60,8 @@ class TestDataHandler:
     @mongomock.patch(servers=('mongodb://localhost:27017'))
     def test_set_patient_specific_data(self, *args):
         insert_data_to_database()
-        data_handler = get_data_handler(cache=True)
-        with patch.object(DataHandler, 'get_patient_info_from_mongo',
+        data_handler = get_patient_data_handler(cache=True)
+        with patch.object(PatientDataHandler, 'get_patient_info_from_mongo',
                           wraps=data_handler.get_patient_info_from_mongo) as mongo_spy:
             data_handler.fetch_patient_specific_data(patient='TCGA-06-2557',
                                                      modality='mRNA',
@@ -52,7 +75,7 @@ class TestDataHandler:
             mongo_spy.assert_not_called()
             mongo_spy.reset_mock()
 
-            data_handler = get_data_handler(cache=True, force_cache=True)
+            data_handler = get_patient_data_handler(cache=True, force_cache=True)
             data_handler.fetch_patient_specific_data(patient='TCGA-06-2557',
                                                      modality='mRNA',
                                                      features=['TSPAN6'])
@@ -64,6 +87,157 @@ class TestDataHandler:
                                                      features=['TNMD'])
             mongo_spy.assert_called_once()
 
-    def test_generate_hash(self, *args):
-        assert DataHandler.generate_patient_modality_feature_hash(patient='TCGA-06-2557', modality='mRNA',
-                                                                  feature='TNMD') == 'TCGA-06-2557-mRNA-TNMD'
+    def test_generate_patient_modality_hash(self, *args):
+        assert PatientDataHandler.generate_patient_modality_feature_hash(patient='TCGA-06-2557', modality='mRNA',
+                                                                         feature='TNMD') == 'TCGA-06-2557-mRNA-TNMD'
+
+
+@patch.object(redis, 'Redis', return_value=dict(abc=True))
+class TestFeatureDataHandler:
+    def test_generate_feature_modality_hash(self, *args):
+        assert FeatureDataHandler.generate_feature_modality_hash(modality='mRNA', feature='TNMD') == 'mRNA-TNMD'
+
+    @mongomock.patch(servers=('mongodb://localhost:27017'))
+    def test_get_feature_specific_data(self, *args):
+        insert_data_to_database()
+
+        data_handler = get_feature_data_handler()
+
+        data = data_handler.fetch_feature_specific_data(feature='TNMD', modality='mRNA')
+        np.testing.assert_equal(data, np.array([0.219, 0.1109]))
+
+    @mongomock.patch(servers=('mongodb://localhost:27017'))
+    def test_set_feature_specific_data(self, *args):
+        insert_data_to_database()
+        data_handler = get_feature_data_handler(cache=True)
+        with patch.object(FeatureDataHandler, 'get_feature_info_from_mongo',
+                          wraps=data_handler.get_feature_info_from_mongo) as mongo_spy:
+            data_handler.fetch_feature_specific_data(modality='mRNA',
+                                                     feature='TNMD')
+            mongo_spy.assert_called()
+            mongo_spy.reset_mock()
+
+            data_handler.fetch_feature_specific_data(modality='mRNA',
+                                                     feature='TNMD')
+            mongo_spy.assert_not_called()
+            mongo_spy.reset_mock()
+
+            data_handler = get_feature_data_handler(cache=True, force_cache=True)
+            data_handler.fetch_feature_specific_data(modality='mRNA',
+                                                     feature='TNMD')
+            mongo_spy.assert_called_once()
+            mongo_spy.reset_mock()
+
+            data_handler.fetch_feature_specific_data(modality='mRNA',
+                                                     feature='TSPAN6')
+            mongo_spy.assert_called_once()
+
+
+class TestFeaturesMetadataHandler:
+
+    @pytest.mark.parametrize('patients,fetch_result', [(None, [{'feature': 'TNMD',
+                                                                'metadata': {'max': 0.219, 'min': 0.1109, 'stdev': 2,
+                                                                             'avg': 0.16494999999999999, 'var': 4.0}}]),
+                                                       ('TCGA-06-2557', [{'feature': 'TNMD',
+                                                                          'metadata': {'max': 0.219,
+                                                                                       'min': 0.219,
+                                                                                       'stdev': 1,
+                                                                                       'avg': 0.219,
+                                                                                       'var': 1.0}}])])
+    def test_fetch_features_metadata(self, patients, fetch_result, *args):
+        with mongomock.patch(servers=('mongodb://localhost:27017')):
+            insert_data_to_database()
+            with patch.object(redis, 'Redis', return_value=dict(abc=True)), patch.object(
+                    FeaturesMetadataHandler, 'get_metadata_aggregation_values',
+                    return_value={
+                        '$group': {
+                            '_id': '$name',
+                            'max': {
+                                '$max': '$value'
+                            },
+                            'min': {
+                                '$min': '$value'
+                            },
+                            'stdev': {
+                                '$sum': 1
+                            },
+                            'avg': {
+                                '$avg': '$value'
+                            }
+                        }
+                    }):
+                data_handler = get_features_metadata_handler(cache=True)
+                with patch.object(FeaturesMetadataHandler, 'get_features_metadata_from_mongo',
+                                  wraps=data_handler.get_features_metadata_from_mongo) as mongo_spy:
+                    results = data_handler.fetch_features_metadata(modality='mRNA',
+                                                                   features=['TNMD'],
+                                                                   patients=patients)
+                    assert results == fetch_result
+                    mongo_spy.assert_called()
+                    mongo_spy.reset_mock()
+
+                    results = data_handler.fetch_features_metadata(modality='mRNA',
+                                                                   features=['TNMD'],
+                                                                   patients=patients)
+                    assert results == fetch_result
+                    mongo_spy.assert_not_called()
+                    mongo_spy.reset_mock()
+
+                    data_handler = get_features_metadata_handler(cache=True, force_cache=True)
+                    results = data_handler.fetch_features_metadata(modality='mRNA',
+                                                                   features=['TNMD'],
+                                                                   patients=patients)
+                    assert results == fetch_result
+                    mongo_spy.assert_called_once()
+                    mongo_spy.reset_mock()
+
+                    results = data_handler.fetch_features_metadata(modality='mRNA',
+                                                                   features=['TSPAN6'],
+                                                                   patients=patients)
+                    mongo_spy.assert_called_once()
+
+    def test_generate_feature_modality_metadata_hash(self):
+        assert FeaturesMetadataHandler.generate_feature_modality_metadata_hash(modality='mRNA',
+                                                                               feature='TNMD') == 'mRNA-TNMD-metadata'
+
+
+@mongomock.patch(servers=('mongodb://localhost:27017'))
+@patch.object(redis, 'Redis', return_value=dict(abc=True))
+def test_generate_metadata(*args):
+    with TemporaryDirectory() as t, patch.object(
+            FeaturesMetadataHandler, 'get_metadata_aggregation_values',
+            return_value={
+                '$group': {
+                    '_id': '$name',
+                    'max': {
+                        '$max': '$value'
+                    },
+                    'min': {
+                        '$min': '$value'
+                    },
+                    'stdev': {
+                        '$sum': 1
+                    },
+                    'avg': {
+                        '$avg': '$value'
+                    }
+                }
+            }):
+        insert_data_to_database()
+
+        with Path(t, 'patients.csv').open('w') as f:
+            csv.writer(f).writerow(['TCGA-06-2557', 'TCGA-06-2559'])
+
+        with Path(t, 'features.csv').open('w') as f:
+            f.write('TSPAN6')
+
+        metadata = generate_metadata(mongodb_connection_string='mongodb://localhost:27017',
+                                     redis_host='redis://localhost:6379',
+                                     modality='mRNA',
+                                     patients_file=Path(t, 'patients.csv'),
+                                     features_file=Path(t, 'features.csv'),
+                                     db_name='TCGAOmics'
+                                     )
+
+        assert metadata == {'feature': 'TSPAN6',
+                            'metadata': {'max': 16.7648, 'min': 14.05, 'stdev': 2, 'avg': 15.4074, 'var': 4.0}}
